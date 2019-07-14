dd99c702f9ccf6c557fb744dcee0e9b6d613dff1
Ross Lantea <47453314+RossLantea@users.noreply.github.com>
2019-07-14 20:38:09 +0300

    Add files via upload

---
+++ python_app_webmap/map.py
@@ -0,0 +1,43 @@
+﻿import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lon = list(data["LON"])
+lat = list(data["LAT"])
+elev = list(data["ELEV"])
+name = list(data["NAME"])
+
+def color(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'gray'
+    else:
+        return 'red'
+
+html = """
+Volcano name:<br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+Height: %s m
+"""
+map = folium.Map(location=[37.765122, -122.427284], zoom_start=6, tiles="Mapbox Bright")
+
+fgv = folium.FeatureGroup(name="Volcanoes")
+
+#zipin avulla käydään läpi kaksi listaa: eka lat ja sitten lon
+
+for lt, ln, el, name in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
+    fgv.add_child(folium.Marker(location=[lt, ln],  popup=folium.Popup(iframe), icon=folium.Icon(color=color(el))))
+
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'yellow' if x['properties']['POP2005'] < 10000000
+else 'yellow' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+
+map.save("Map1_html_popup_advanced.html")
